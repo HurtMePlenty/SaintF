a2d330e4320d2dd34d3480f08cf16a61e8e52377
Eliphas <eliphas@Mac-mini-Eliphas.local>
2014-04-22 02:08:46 +0400

    Initial Commit

---
+++ SaintF/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  SaintF
+//
+//  Created by Eliphas on 22.04.14.
+//  Copyright Eliphas 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
